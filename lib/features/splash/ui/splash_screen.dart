beffc45f681d64cd9fb9dc0f6d9ac76e92819753
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-14 17:52:41 +0300

    add splash screen by using animated splash package

---
+++ lib/features/splash/ui/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:foot_fire/core/routing/routes.dart';
+import 'package:foot_fire/core/theming/app_colors.dart';
+import 'package:foot_fire/features/onboarding/ui/on_boarding_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: AnimatedSplashScreen(
+        splash: Image.asset("assets/images/logo.png"),
+        splashIconSize: 280,
+        backgroundColor: AppColors.darkGrayColor,
+        nextRoute: Routes.onBoarding,
+        disableNavigation: false,
+        duration: 400,
+        animationDuration: const Duration(seconds: 1),
+        splashTransition: SplashTransition.scaleTransition,
+        nextScreen: const OnboardingScreen(),
+      ),
+    );
+  }
+}
