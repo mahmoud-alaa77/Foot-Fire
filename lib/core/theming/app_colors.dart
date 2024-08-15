a25f8386a9342c52cfde57fce7a4d5991c289d38
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-15 21:27:38 +0300

    change colors and add text styles

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color customBlackColor = Color(0xFF222831);
-    static const Color darkGrayColor = Color(0xFF31363F);
-
+  static const Color backGroundBlackColor = Color(0xFF0F0F0F);
+  static const Color orangeColor = Color(0xFFfeb800);
+  static const Color darkGrayColor = Color(0xFF1c1c1e);
 }
---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextStyles {
+  static  TextStyle font24WhiteW700 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.white
+  );
+  static  TextStyle font18WhiteW700 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.white
+  );
+
+static  TextStyle font16WhiteW500 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+    color: Colors.white
+  );
+
+
+  static  TextStyle font24BlackW700 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black
+  );
+    static  TextStyle font18BlackW700 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black
+  );
+  
+}
