3dfcfa865d39ebcb09308d2b2ac1e0379d240a6b
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-15 22:16:19 +0300

    provide get it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:foot_fire/core/dependancy_injection/di.dart';
 import 'package:foot_fire/core/routing/app_routing.dart';
 import 'package:foot_fire/foot_fire_app.dart';
 
 void main() {
+  setupGetIt();
   runApp(FootFireApp(
     appRouter: AppRouting(),
   ));
