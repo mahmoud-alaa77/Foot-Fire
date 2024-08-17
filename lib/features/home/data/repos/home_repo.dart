e808a2e74e608270c85c67123aab5f0469ca64bc
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-17 20:06:44 +0300

    add getAllLeaguesByCountryName function to home repo class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -4,6 +4,7 @@ import 'package:foot_fire/core/errors/failure.dart';
 import 'package:foot_fire/core/errors/server_failure.dart';
 import 'package:foot_fire/core/networking/api_services.dart';
 import 'package:foot_fire/features/home/data/models/country_model.dart';
+import 'package:foot_fire/features/home/data/models/league_model.dart';
 
 class HomeRepo {
   final ApiServices _apiServices;
@@ -13,6 +14,21 @@ class HomeRepo {
   Future<Either<Failure, CountryModel>> getAllCountries() async {
     try {
       var response = await _apiServices.getAllCountries();
+
+      return right(response);
+    } catch (error) {
+      if (error is DioException) {
+        return left(ServerFailure.fromDioError(error));
+      } else {
+        return left(Failure(error.toString()));
+      }
+    }
+  }
+
+  Future<Either<Failure, LeagueModel>> getAllLeaguesByCountryName(
+      String countryName) async {
+    try {
+      var response = await _apiServices.getLeaguesByCountryName(countryName);
 
       return right(response);
     } catch (error) {
