3af698eb98e45ac895d69b81e8b0cd66138c62d4
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-20 19:14:35 +0300

    provide table cubit

--- lib/core/dependancy_injection/di.dart
+++ lib/core/dependancy_injection/di.dart
@@ -3,6 +3,8 @@ import 'package:foot_fire/core/networking/api_services.dart';
 import 'package:foot_fire/features/home/data/repos/home_repo.dart';
 import 'package:foot_fire/features/home/logic/cubit/country_cubit.dart';
 import 'package:foot_fire/features/home/logic/cubit/league_cubit.dart';
+import 'package:foot_fire/features/table/data/repos/table_repo.dart';
+import 'package:foot_fire/features/table/logic/cubit/table_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -20,6 +22,12 @@ Future<void> setupGetIt() async {
 
   getIt.registerFactory<CountryCubit>(() => CountryCubit(getIt()));
     getIt.registerFactory<LeagueCubit>(() => LeagueCubit(getIt()));
+
+    //league table
+
+  getIt.registerLazySingleton<TableRepo>(() => TableRepo(getIt()));
+
+  getIt.registerFactory<TableCubit>(() => TableCubit(getIt()));
 
 }
 
