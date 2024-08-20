9e61a1cc043735fd841bbbefd3e96cb10fda6b6f
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-20 19:15:23 +0300

    add featch table function to api service class

--- lib/core/networking/api_services.dart
+++ lib/core/networking/api_services.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:foot_fire/core/networking/api_constants.dart';
 import 'package:foot_fire/features/home/data/models/country_model.dart';
 import 'package:foot_fire/features/home/data/models/league_model.dart';
+import 'package:foot_fire/features/table/data/models/table_model.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 
@@ -17,4 +18,8 @@ abstract class ApiServices {
   @GET("/api/v1/json/3/search_all_leagues.php?c={countryName}")
   Future<LeagueModel> getLeaguesByCountryName(
       @Path("countryName") String countryName);
+
+  @GET("/api/v1/json/3/lookuptable.php?l={leagueId}&s={season}")
+  Future<TableModel> getTableByLeagueIdAndSeason(
+      @Path("leagueId") String leagueId, @Path("season") String season);
 }
--- lib/core/networking/api_services.g.dart
+++ lib/core/networking/api_services.g.dart
@@ -89,6 +89,42 @@ class _ApiServices implements ApiServices {
     return _value;
   }
 
+  @override
+  Future<TableModel> getTableByLeagueIdAndSeason(
+    String leagueId,
+    String season,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<TableModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/api/v1/json/3/lookuptable.php?l=${leagueId}&s=${season}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late TableModel _value;
+    try {
+      _value = TableModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
