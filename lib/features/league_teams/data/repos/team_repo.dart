02078153b11048e7ea69b7a7c05f93cc357d3add
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-21 18:41:44 +0300

    add team repo

---
+++ lib/features/league_teams/data/repos/team_repo.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:foot_fire/core/errors/server_failure.dart';
+import 'package:foot_fire/core/networking/api_services.dart';
+import 'package:foot_fire/features/league_teams/data/models/team_model.dart';
+
+import '../../../../core/errors/failure.dart';
+
+class TeamRepo {
+  final ApiServices _apiServices;
+
+  TeamRepo(this._apiServices);
+
+  Future<Either<Failure, TeamModel>> getTeams(String leagueName) async {
+    try {
+      var response =
+          await _apiServices.getTeamsInLeagueByLeagueName(leagueName);
+
+      return right(response);
+    } catch (error) {
+      if (error is DioException) {
+        return left(ServerFailure.fromDioError(error));
+      } else {
+        return left(Failure(error.toString()));
+      }
+    }
+  }
+}
