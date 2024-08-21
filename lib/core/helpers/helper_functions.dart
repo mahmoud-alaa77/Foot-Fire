040250af7b51ab0f77fd9a4701c1017a0190a5ce
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-21 17:26:35 +0300

    add function forconvert 24h to 12h

--- lib/core/helpers/helper_functions.dart
+++ lib/core/helpers/helper_functions.dart
@@ -3,7 +3,8 @@ import 'dart:math';
 import 'package:easy_url_launcher/easy_url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:foot_fire/core/helpers/constants.dart';
-
+import 'package:foot_fire/core/theming/app_colors.dart';
+import 'package:intl/intl.dart';
 class HelperFunctions {
   String getImageUrl() {
     if (animatedImages.isEmpty) {
@@ -28,10 +29,22 @@ class HelperFunctions {
       await EasyLauncher.url(url: "https://$url", mode: Mode.platformDefault);
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('There was an error, try again later'),
+         const SnackBar(
+          content: Text('This page not available'),
+          backgroundColor: AppColors.lightGrayColor,
+          
         ),
       );
     }
   }
+
+
+String convertTo12HourFormat(String timeString) {
+  final format24 = DateFormat('HH:mm:ss');
+  final format12 = DateFormat('hh:mm a');
+  DateTime time = format24.parse(timeString);
+  time = time.add(const Duration(hours: 2));
+  return format12.format(time);
+}
+
 }
