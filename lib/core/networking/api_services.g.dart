f79c2e267ee4e6fc34fb15caf8b3c9546ff32fe2
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-27 17:57:52 +0300

    add get honours functions to api services class

--- lib/core/networking/api_services.g.dart
+++ lib/core/networking/api_services.g.dart
@@ -326,6 +326,39 @@ class _ApiServices implements ApiServices {
     return _value;
   }
 
+  @override
+  Future<HonourModel> getPlayerHonoursByPlayerId(String playerID) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<HonourModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/api/v1/json/3/lookuphonours.php?id=${playerID}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late HonourModel _value;
+    try {
+      _value = HonourModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
