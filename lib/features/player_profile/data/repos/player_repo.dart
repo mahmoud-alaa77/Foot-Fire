810540bdf0952d41ba9a6e4e1f48c9c6271bd7b4
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-08-28 11:30:47 +0300

    add new fun to player repo file to fetch player career

--- lib/features/player_profile/data/repos/player_repo.dart
+++ lib/features/player_profile/data/repos/player_repo.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:foot_fire/core/errors/failure.dart';
 import 'package:foot_fire/core/errors/server_failure.dart';
 import 'package:foot_fire/core/networking/api_services.dart';
+import 'package:foot_fire/features/player_profile/data/models/career_model.dart';
 import 'package:foot_fire/features/player_profile/data/models/honour_model.dart';
 
 class PlayerRepo {
@@ -24,4 +25,20 @@ class PlayerRepo {
       }
     }
   }
+
+Future<Either<Failure, CareerModel>> getPlayerCareerByPlayerID(
+      String playerId) async {
+    try {
+      var response = await _apiServices.getPlayerCareerByPlayerId(playerId);
+
+      return right(response);
+    } catch (error) {
+      if (error is DioException) {
+        return left(ServerFailure.fromDioError(error));
+      } else {
+        return left(Failure(error.toString()));
+      }
+    }
+  }
+
 }
