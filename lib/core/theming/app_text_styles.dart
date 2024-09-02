1908a07d4fd753433f831dcd4eb57631ce24e95f
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-09-02 03:08:50 +0300

    add new text styles

--- lib/core/theming/app_text_styles.dart
+++ lib/core/theming/app_text_styles.dart
@@ -1,90 +1,54 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foot_fire/core/theming/app_colors.dart';
 
 class AppTextStyles {
-  static  TextStyle font24WhiteW700 = TextStyle(
-    fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
-    color: Colors.white
-  );
-   static  TextStyle font12WhiteW300 = TextStyle(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.w300,
-    color: Colors.white
-  );
+  static TextStyle font24WhiteW700 = TextStyle(
+      fontSize: 24.sp, fontWeight: FontWeight.w700, color: Colors.white);
+  static TextStyle font12WhiteW300 = TextStyle(
+      fontSize: 12.sp, fontWeight: FontWeight.w300, color: Colors.white);
 
+  static TextStyle font18WhiteW700 = TextStyle(
+      fontSize: 18.sp, fontWeight: FontWeight.w700, color: Colors.white);
+  static TextStyle font18WhiteW400 = TextStyle(
+      fontSize: 18.sp, fontWeight: FontWeight.w400, color: Colors.white);
 
+  static TextStyle font20WhiteW800 = TextStyle(
+      fontSize: 20.sp, fontWeight: FontWeight.w800, color: Colors.white);
 
-  static  TextStyle font18WhiteW700 = TextStyle(
-    fontSize: 18.sp,
-    fontWeight: FontWeight.w700,
-    color: Colors.white
-  );
-   static  TextStyle font18WhiteW400 = TextStyle(
-    fontSize: 18.sp,
-    fontWeight: FontWeight.w400,
-    color: Colors.white
-  );
+  static TextStyle font16WhiteW500 = TextStyle(
+      fontSize: 16.sp, fontWeight: FontWeight.w500, color: Colors.white);
 
+  static TextStyle font14WhiteW500 = TextStyle(
+      fontSize: 14.sp, fontWeight: FontWeight.w500, color: Colors.white);
 
-  static  TextStyle font20WhiteW800 = TextStyle(
-    fontSize: 20.sp,
-    fontWeight: FontWeight.w800,
-    color: Colors.white
-  );
+  static TextStyle font16BlackW300 = TextStyle(
+      fontSize: 16.sp, fontWeight: FontWeight.w300, color: Colors.black);
 
-static  TextStyle font16WhiteW500 = TextStyle(
-    fontSize: 16.sp,
-    fontWeight: FontWeight.w500,
-    color: Colors.white
-  );
+  static TextStyle font14OrangeW400 = TextStyle(
+      fontSize: 14.sp,
+      fontWeight: FontWeight.w400,
+      color: AppColors.orangeColor);
+  static TextStyle font18OrangeW400 = TextStyle(
+      fontSize: 18.sp,
+      fontWeight: FontWeight.w400,
+      color: AppColors.orangeColor);
+  static TextStyle font22OrangeW400 = TextStyle(
+      fontSize: 22.sp,
+      fontWeight: FontWeight.w400,
+      color: AppColors.orangeColor);
+  static TextStyle font14GreyW400 = TextStyle(
+      fontSize: 14.sp,
+      fontWeight: FontWeight.w400,
+      color: AppColors.lightGrayColor);
 
-static  TextStyle font14WhiteW500 = TextStyle(
-    fontSize: 14.sp,
-    fontWeight: FontWeight.w500,
-    color: Colors.white
-  );
+  static TextStyle font16GreyW400 = TextStyle(
+      fontSize: 16.sp,
+      fontWeight: FontWeight.w400,
+      color: AppColors.lightGrayColor);
 
-  static  TextStyle font16BlackW300 = TextStyle(
-    fontSize: 16.sp,
-    fontWeight: FontWeight.w300,
-    color: Colors.black
-  );
-
-static  TextStyle font14OrangeW400 = TextStyle(
-    fontSize: 14.sp,
-    fontWeight: FontWeight.w400,
-    color: AppColors.orangeColor
-  );
-  static  TextStyle font22OrangeW400 = TextStyle(
-    fontSize: 22.sp,
-    fontWeight: FontWeight.w400,
-    color: AppColors.orangeColor
-  );
-  static  TextStyle font14GreyW400 = TextStyle(
-    fontSize: 14.sp,
-    fontWeight: FontWeight.w400,
-    color: AppColors.lightGrayColor
-  );
-
-   static  TextStyle font16GreyW400 = TextStyle(
-    fontSize: 16.sp,
-    fontWeight: FontWeight.w400,
-    color: AppColors.lightGrayColor
-  );
-
-  static  TextStyle font24BlackW700 = TextStyle(
-    fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
-    color: Colors.black
-  );
-    static  TextStyle font18BlackW700 = TextStyle(
-    fontSize: 18.sp,
-    fontWeight: FontWeight.w700,
-    color: Colors.black
-  );
-  
+  static TextStyle font24BlackW700 = TextStyle(
+      fontSize: 24.sp, fontWeight: FontWeight.w700, color: Colors.black);
+  static TextStyle font18BlackW700 = TextStyle(
+      fontSize: 18.sp, fontWeight: FontWeight.w700, color: Colors.black);
 }
